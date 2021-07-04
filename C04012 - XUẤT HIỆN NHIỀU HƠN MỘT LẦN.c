beaa14be66634b3639d36e86bdf895ba03ae2d99
nguyenvantu11052002 <86882000+nguyenvantu11052002@users.noreply.github.com>
2021-07-04 20:26:17 +0700

    Create C04012 - XUẤT HIỆN NHIỀU HƠN MỘT LẦN.c

---
+++ C04012 - XUẤT HIỆN NHIỀU HƠN MỘT LẦN.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	int a[1000], b[1000] = {0};
+	for(int i = 0; i < n; i++){
+		scanf("%d", &a[i]);
+		b[a[i]]++;
+	}
+	for(int i = 0; i < n; i++){
+		if(b[a[i]] > 1){
+			printf("%d ", a[i]);
+			b[a[i]] = 0;
+		}
+	}
+	return 0;
+}
