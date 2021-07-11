49d4b0f7c8bbf9101670da03b3b0dc02393d1e14
nguyenvantu11052002 <86882000+nguyenvantu11052002@users.noreply.github.com>
2021-07-11 19:40:50 +0700

    Create C02003 - HÌNH VUÔNG RỖNG VỚI DẤU *.c

---
+++ C02003 - HÌNH VUÔNG RỖNG VỚI DẤU *.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+
+void inraketqua(){
+	int n;
+	scanf("%d", &n);
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= n; j++){
+			if(i == 1 || i == n) printf("*");
+			else{
+				if(j == 1 || j == n) printf("*");
+				else printf(".");
+			}
+		}
+		printf("\n");
+	}
+}
+
+int main(){
+	inraketqua();
+	return 0;
+}
